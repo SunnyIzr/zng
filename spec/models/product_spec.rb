086c385221acbadc29c56017bd1f3487684b4051
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2013-12-04 15:34:34 -0500

    Establish shoulda tests to ensure uniqueness of product.sku, color and pattern

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,6 +5,7 @@ describe Product do
   it {should validate_presence_of (:sku)}
   it {should validate_presence_of (:title)}
   it {should validate_presence_of (:image)}
+  it {should validate_uniqueness_of(:sku)}
   it {should belong_to (:color)}
   it {should belong_to (:pattern)}
 end
