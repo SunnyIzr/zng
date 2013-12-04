53f264ce014acb774d22b37fa36b788bcad88e64
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2013-12-04 18:38:21 -0500

    Create users table in db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131204194037) do
+ActiveRecord::Schema.define(version: 20131204212241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,5 +34,12 @@ ActiveRecord::Schema.define(version: 20131204194037) do
 
   add_index "products", ["color_id"], name: "index_products_on_color_id", using: :btree
   add_index "products", ["pattern_id"], name: "index_products_on_pattern_id", using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
