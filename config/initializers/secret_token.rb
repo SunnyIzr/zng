440614165282dad4d2739f42a54d33d15a4d4431
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2013-12-04 11:32:12 -0500

    Rails Boilerplate Code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zng::Application.config.secret_key_base = '33c2af52564538823f6edddf47e7bf24473ab57822320ee45c65c5ea8186e22fa4c914d8e5c8bc32422d80311a1216e1c07e715d9e207ddb08502166988d3913'
