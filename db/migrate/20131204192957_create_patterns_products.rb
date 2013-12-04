44f3dbcbfbfcb7ee16723513f1e87006af31b0ba
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2013-12-04 14:39:44 -0500

    Remove unneccessary join table migrations

--- db/migrate/20131204192957_create_patterns_products.rb
+++
@@ -1,8 +0,0 @@
-class CreatePatternsProducts < ActiveRecord::Migration
-  def change
-    create_table :patterns_products do |t|
-      t.belongs_to :pattern
-      t.belongs_to :product
-    end
-  end
-end
