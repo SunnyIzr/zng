d2cafcd966e4f7a7574d05bf6665d9db9a932d92
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2013-12-04 15:34:55 -0500

    Correct uniqueness validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ActiveRecord::Base
   validates_presence_of :sku
   validates_presence_of :title
   validates_presence_of :image
-  validates_presence_of :product
+  validates_uniqueness_of :sku
   belongs_to :color
   belongs_to :pattern
 end
