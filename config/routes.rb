8a2e4f97f7fc91ea24c6dcc05fe6d6b55b574fcc
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2013-12-04 19:10:58 -0500

    Add routes to faciliated signing in/signing out with FB-Omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Zng::Application.routes.draw do
 
   root 'static_pages#index'
+
+  match 'auth/:provider/callback', to: 'sessions#create', via: [:get,:post]
+  match 'auth/failure', to: redirect('/'), via: [:get,:post]
+  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get,:post]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
