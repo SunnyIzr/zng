676c4efa89b3594c3210fece59b3b64e535a4d7a
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2013-12-05 18:05:01 -0500

    Route products index

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Zng::Application.routes.draw do
   match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get,:post]
 
   get '/query' => 'products#query'
+
+  resources :products, only: [:index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
